6cecf6e38b9207bb3955ea15e87f24dd28428f90
Jkrzzz <heinhtetjkrz@gmail.com>
2025-09-02 21:42:09 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // ensure correct path on Vercel
+  build: {
+    outDir: "dist", // Vite default, just to be explicit
+  },
+});
